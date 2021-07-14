ab96b1a7cab5f82274356b227a6bd303de1bb678
Eduardo da Silva <eduardo.silva@gmail.com>
2021-07-14 20:11:20 -0300

    Lista de Tarefas - Vue-cli

---
+++ vue-cli/lista-tarefas/src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="container">
+    <h2>{{ titulo }}</h2>
+    <div class="input-group">
+      <input @keyup.enter="adicionarTarefa" v-model="novaTarefa" type="text" />
+      <span class="input-group-btn">
+        <button @click="adicionarTarefa" class="btn btn-primary">
+          Adicionar tarefa
+        </button>
+      </span>
+    </div>
+    <ul>
+      <li
+        v-for="(tarefa, index) in tarefas"
+        :key="index"
+        :class="{ removed: tarefa.checked }"
+      >
+        <input type="checkbox" v-model="tarefa.checked" />
+        <label>{{ tarefa.titulo }}</label>
+      </li>
+    </ul>
+    <footer>
+      <em>Altere aqui o título da sua lista de tarefas</em>
+      <input v-model="titulo" type="text" />
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      titulo: "Minha lista de tarefas",
+      novaTarefa: "",
+      tarefas: [
+        { titulo: "Estudar", checked: false },
+        { titulo: "Programar", checked: true },
+      ],
+    };
+  },
+  methods: {
+    adicionarTarefa() {
+      this.tarefas.push({
+        titulo: this.novaTarefa,
+        checked: false,
+      });
+      this.novaTarefa = "";
+    },
+  },
+};
+</script>
+
+<style>
+.container {
+  width: 40%;
+  margin: 20px auto 0px auto;
+}
+
+ul li {
+  list-style: none;
+}
+
+.removed {
+  color: gray;
+}
+
+.removed label {
+  text-decoration: line-through;
+}
+</style>
